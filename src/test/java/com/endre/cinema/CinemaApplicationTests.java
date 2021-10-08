de7de13ca6e2dbe92259f79fab3fc63416f18785
Endre Synnes <endre.m.synnes@gmail.com>
2021-10-08 09:12:02 +0200

    removed redundant mvn files and updated spring boot to 2.5.5

--- src/test/java/com/endre/cinema/CinemaApplicationTests.java
+++ src/test/java/com/endre/cinema/CinemaApplicationTests.java
@@ -1,16 +1,16 @@
 package com.endre.cinema;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class CinemaApplicationTests {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)@SpringBootTest
+class CinemaApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
