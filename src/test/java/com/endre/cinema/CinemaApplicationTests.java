fd4ff0a348796027a72843b0d0ba0275984c887f
Endre Synnes <endre.m.synnes@gmail.com>
2021-10-08 15:18:08 +0200

    removed extendwith in tests

--- src/test/java/com/endre/cinema/CinemaApplicationTests.java
+++ src/test/java/com/endre/cinema/CinemaApplicationTests.java
@@ -1,17 +1,13 @@
 package com.endre.cinema;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class CinemaApplicationTests {
 
